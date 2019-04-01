d739eab7cfa893a34f39c6c31e008981cb91f5b2
Jay <jhar226@lsu.edu>
2019-04-01 10:34:18 -0500

    add post works and save works

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import PostForm
+from django.utils import timezone
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.order_by('published_date')
@@ -11,5 +13,14 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
